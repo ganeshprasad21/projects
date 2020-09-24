2195a9963f02feb4160f85b4581a2304aceef284
ganeshprasad21 <ganesh21111999@gmail.com>
2020-09-25 01:44:21 +0530

    new commit

--- filecrawler_to_find_words_in_c_h_file/scripts/find_func_in_a_file.py
+++ filecrawler_to_find_words_in_c_h_file/scripts/find_func_in_a_file.py
@@ -4,5 +4,5 @@ def find_function(file_to_search,function_list,answerFile):
 		with open(file_to_search) as fileToSearch:
 			for line in fileToSearch:
 				if function in line.rstrip(): #read line by line
-					answerFile.write("\n"+"in the file :::  "+ "\n" +file_to_search.split(r"/")[-1] +"<<<>>>"+"\n\n"+" found function-name :::   "+function+"   ::: at line ::: "+ str(line_num)+ "\n")
+					answerFile.write("\n"+"in the file :::  "+ "\n" +file_to_search +"<<<>>>"+"\n\n"+"    found function-name :::      "+function+"   ::: at    line ::: "+ str(line_num)+ "\n")
 				line_num += 1
